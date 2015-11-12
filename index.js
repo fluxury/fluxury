d2f6a70cec87f655ca8fc9ca59249f5a2b6a6d6f
JC Fisher <jcfisher007@gmail.com>
2015-11-11 21:51:32 -0800

    v0.2.1

--- index.js
+++ index.js
@@ -3,45 +3,47 @@ import {Dispatcher} from 'flux';
 var dispatcher = new Dispatcher();
 var changedEvent = 'change';
 
-export default function Fluxury(config) {
-  return Object.freeze({
-    /* thin bridge to internal dispatcher */
-    dispatch: function(action) {
-      dispatcher.dispatch(action)
-    },
-    /* transform a list of actions into useful functions */
-    createActions: function(actions) {
-      return actions.reduce(function(a,b) {
-        return a[b] = function(data) {
-          return dispatcher.dispatch({ type: b, data: data })), {});
-        }
+export default Object.freeze({
+
+  /* thin bridge to internal dispatcher */
+  dispatch: function(action) {
+    dispatcher.dispatch(action)
+  },
+
+  /* transform a list of actions into useful functions */
+  createActions: function(actions) {
+    return actions.reduce(function(a,b) {
+      a[b] = b;
+      return a;
+    }, {});
+  },
+
+  /* create a named store with an initialState and a reducer to move it forward */
+  createStore: function(name, initialState, reducer, waitFor=[]) {
+    var currentState = initialState;
+    var emitter = new EventEmitter();
+    var subscription;
+
+    var dispatchToken = dispatcher.register( function(action) {
+      dispatcher.waitFor(waitFor);
+      var newState = reducer(currentState, action);
+      if (currentState !== newState) {
+        currentState = newState;
+        emitter.emit(changedEvent);
       }
-    },
-    /* create a named store with an initialState and a reducer to move it forward */
-    createStore: function(name, initialState, reducer, waitFor=[]) {
-      var currentState = {};
-      var emitter = new EventEmitter();
-      var subscription;
+    });
 
-      var dispatchToken = dispatcher.register( function(action) {
-        dispatcher.waitFor(waitFor);
-        var newState = reducer(currentState, action);
-        if (currentState !== newState) {
-          emitter.emit(changedEvent);
-        }
-      });
+    return Object.freeze({
+      name: name,
+      dispatchToken: dispatchToken,
+      addListener: function(cb) {
+        return emitter.addListener(changedEvent, cb)
+      },
+      getState: function(cb) {
+        return currentState;
+      }
+    });
 
-      return Object.freeze({
-        name: name,
-        dispatchToken: dispatchToken,
-        addListener: function(cb) {
-          return emitter.addListener(changedEvent, cb)
-        },
-        getState: function(cb) {
-          return currentState;
-        }
-      });
+  }
 
-    }
-  })
-}
+});
