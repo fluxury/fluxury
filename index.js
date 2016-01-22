8d3c9ebfb877fc1cd5cce8952a8e7cc9264d6e31
Peter Moresi <peter.moresi@gmail.com>
2016-01-21 19:29:53 -0800

    Add more sugar

--- index.js
+++ index.js
@@ -43,22 +43,22 @@ if (!Object.assign) {
 // ES5 15.2.3.9
 // http://es5.github.com/#x15.2.3.9
 if (!Object.freeze) {
-    Object.freeze = function freeze(object) {
-        if (Object(object) !== object) {
-            throw new TypeError('Object.freeze can only be called on Objects.');
-        }
-        // this is misleading and breaks feature-detection, but
-        // allows "securable" code to "gracefully" degrade to working
-        // but insecure code.
-        return object;
-    };
+  Object.freeze = function freeze(object) {
+    if (Object(object) !== object) {
+      throw new TypeError('Object.freeze can only be called on Objects.');
+    }
+    // this is misleading and breaks feature-detection, but
+    // allows "securable" code to "gracefully" degrade to working
+    // but insecure code.
+    return object;
+  };
 }
 
 let dispatcher = new Dispatcher(),
-  changedEvent = 'change',
-  waitFor = dispatcher.waitFor.bind(dispatcher);
+changedEvent = 'change',
+waitFor = dispatcher.waitFor.bind(dispatcher);
 
-export default Object.freeze({
+var fluxury = Object.freeze({
 
   /* thin bridge to internal dispatcher */
   dispatch: function(type, data) {
@@ -75,42 +75,71 @@ export default Object.freeze({
   createStore: function(name, initialState, reducer, methods={}) {
     var currentState = Object.freeze(initialState);
     var emitter = new EventEmitter();
+    let actions = {};
+    let reduce = undefined;
 
     if (typeof reducer === 'object') {
-      var reducers = Object.freeze(reducer)
-      reducer = ((state, action) => {
-        if (action && typeof action.type === 'string' && reducers.hasOwnProperty(action.type)) {
-          return reducers[action.type](state, action);
+
+      // construct a reduce method with the object
+      reduce = ((state, action) => {
+        if (action && typeof action.type === 'string' && reducer.hasOwnProperty(action.type)) {
+          return reducer[action.type](state, action);
         }
         return state;
       })
+
+      // create helpful action methods
+      actions = Object.keys(reducer)
+      .reduce((a, b) => {
+        a[b] = (data) => dispatcher.dispatch({
+          type: b,
+          data: data
+        })
+        return a;
+      }, {})
+
+    } else if (typeof reducer === 'function') {
+      reduce = reducer
+    } else {
+      throw new Error('reducer must be object or function')
     }
 
-    return Object.freeze(Object.assign({
-      name: name,
-      dispatchToken: dispatcher.register( function(action) {
-        var newState = reducer(currentState, action, waitFor);
-        if (currentState !== newState) {
-          currentState = Object.freeze(newState);
-          emitter.emit(changedEvent);
-        }
-      } ),
-      addListener: function(cb) {
-        if (typeof cb !== 'function') {
-          throw "Callback must be a function";
-        }
-        return emitter.addListener(changedEvent, cb)
-      },
-      getState: function(cb) {
-        return currentState;
-      }
-    }, Object.keys(methods).reduce(function(a, b, i) {
+    let boundMethods = Object.keys(methods).reduce(function(a, b, i) {
       var newFunc = {};
       newFunc[b] = function(...params) {
         return methods[b](currentState, ...params);
       }
-      return Object.assign({}, a, newFunc)
-    }, {})));
-  }
+      return Object.assign(a, newFunc)
+    }, {})
 
+    return Object.freeze(
+      Object.assign(
+        {},
+        boundMethods,
+        actions,
+        {
+          name: name,
+          dispatchToken: dispatcher.register( function(action) {
+            var newState = reduce(currentState, action, waitFor);
+            if (currentState !== newState) {
+              currentState = Object.freeze(newState);
+              emitter.emit(changedEvent);
+            }
+          }),
+          addListener: function(cb) {
+            if (typeof cb !== 'function') {
+              throw "Callback must be a function";
+            }
+            return emitter.addListener(changedEvent, cb)
+          },
+          getState: function(cb) {
+            return currentState;
+          }
+        }
+      )
+    );
+  }
 });
+
+exports.dispatch = fluxury.dispatch;
+exports.createStore = fluxury.createStore;
