7ad5085f89230e4f3d7b63c86d1dc741644080af
Peter Moresi <peter.moresi@pnmac.com>
2016-02-25 06:59:07 -0800

    Update example

--- examples/CounterStore.js
+++ examples/CounterStore.js
@@ -2,6 +2,5 @@ import {createStore} from '../index';
 
 export default createStore('CountStore', 0, {
   increment: (state) => state + 1,
-  decrement: (state) => state - 1,
-  set: (state, action) => action.data
+  decrement: (state) => state - 1
 })
